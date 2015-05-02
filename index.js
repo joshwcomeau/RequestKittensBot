42ea47f02ffb775abf1ad67fbdd795e561f0b717
Josh Comeau <joshwcomeau@gmail.com>
2015-05-02 10:28:59 -0400

    Implemented a super-basic bot that replies to users that tweet us

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var twitter = require('twitter');
 var util    = require('util');
 var envar   = require('envar');
-var request = require('request');
 
 
-var twit = new twitter({
+var client = new twitter({
   consumer_key:         envar("REQUESTKITTENS_TWITTER_KEY"),
   consumer_secret:      envar("REQUESTKITTENS_TWITTER_SECRET"),
   access_token_key:     envar("REQUESTKITTENS_TWITTER_ACCESS_TOKEN"),
@@ -12,25 +11,37 @@ var twit = new twitter({
 });
 
 
+function replyToUser(username) {
+  var newStatus = "Hi " + username + "!";
+  client.post('statuses/update', {status: newStatus}, function(error, tweet, response){
+    if(error) {
+      console.log(util.inspect(error));
+      throw error;
+    }
+    console.log("Tweet posted: ", tweet);
+    console.log("Raw response object: ", response);
+  });
+};
 
 
-twit.stream('user', {track:'requestkittens'}, function(stream) {
+
+client.stream('user', {track:'requestkittens'}, function(stream) {
+  var msg, sender;
+  
   stream.on('data', function(data) {
-    var msg = data.text;
-    var sender = data.user.screen_name;
+    console.log("Data received:", data);
+
+    // Is this a message sent to us? Might also be generic user data.
+    if ( data.text && data.user.screen_name) {
+      msg = data.text;
+      sender = data.user.screen_name;
+    }
+    
+    // Version 1: just reply "Hi ______!"
+    replyToUser(sender);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
   });
 });
-
-
-
-// var myNameRegex = /@requestkittens/i;
-
-// function isForMe(tweet) {
-//   return tweet.match(myNameRegex);
-// }
-
-// Bot.listen("sendCatPics", isForMe, function(twitter, action, tweet) {
-//   console.log("twitter:", twitter);
-//   console.log("action:", action);
-//   console.log("tweet:", tweet);
-// });
